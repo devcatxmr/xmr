60f16bbd325c1e39ed7ce9c51248719293373783
devcatxmr <65000155+devcatxmr@users.noreply.github.com>
2020-06-15 23:30:59 +0800

    Update nice3.sh

--- nice3.sh
+++ nice3.sh
@@ -6,6 +6,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v5.11.1/xmrig-5.11.1-xenia
 tar -xzvf xmrig-5.11.1-xenial-x64.tar.gz
 cd xmrig-5.11.1
 read -e -p "输入矿工号:" wkid
-./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k -t 18 -B
+./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k -t 32 -B
 kill -9 $(ps -ef|grep xmrig|grep -v grep|awk '{print $2}')
-./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k -t 18 -B
+./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k -t 32 -B
