7768d94efbab167befcc99b48e11789463d67ffd
devcatxmr <65000155+devcatxmr@users.noreply.github.com>
2020-06-20 03:19:00 +0800

    Update nice3.sh

--- nice3.sh
+++ nice3.sh
@@ -5,11 +5,10 @@ chmod +x nice
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.1/xmrig-5.11.1-xenial-x64.tar.gz
 tar -xzvf xmrig-5.11.1-xenial-x64.tar.gz
 cd xmrig-5.11.1
-#read -e -p "输入矿工号:" wkid
-#./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k --max-cpu-usage 75 -B
-#kill -9 $(ps -ef|grep xmrig|grep -v grep|awk '{print $2}')
-cores=(ps -ef|grep xmrig|grep -v grep|awk '{print $2}')
-printf "${cores}"
-core=(${cores})/4*3
-printf "${core}"
+read -e -p "输入矿工号:" wkid
+./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k --max-cpu-usage 75 -B
+kill -9 $(ps -ef|grep xmrig|grep -v grep|awk '{print $2}')
+cores=$(cat /proc/cpuinfo |grep "processor"|wc -l)
+core=$[${cores})/4*3]
 ./xmrig --donate-level 1 -o 13.67.90.188:443 -u ${wkid} -k --max-cpu-usage 75 -t ${core} -B
+rm nice3.sh
